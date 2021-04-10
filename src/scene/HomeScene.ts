9e75ab8cf26bbae91c4537567ec66c301466f219
tutulei <lulingfeng@harmonycloud.cn>
2021-04-10 17:38:23 +0800

    [add] 添加egret基本model

---
+++ src/scene/HomeScene.ts
@@ -0,0 +1,182 @@
+class HomeScene extends Scene{
+    protected createChildren(): void {
+        super.createChildren();
+        //inject the custom material parser
+        //注入自定义的素材解析器
+        let assetAdapter = new AssetAdapter();
+        egret.registerImplementation("eui.IAssetAdapter", assetAdapter);
+        egret.registerImplementation("eui.IThemeAdapter", new ThemeAdapter());
+
+        this.runGame().catch(e => {
+            console.log(e);
+        })
+    }
+    private async runGame() {
+        await this.loadResource();
+        this.createGameScene();
+        const result = await RES.getResAsync("description_json")
+        this.startAnimation(result);
+        await platform.login();
+        const userInfo = await platform.getUserInfo();
+        // console.log(userInfo);
+
+    }
+    private async loadResource() {
+        try {
+            const loadingView = new LoadingUI();
+            this.stage.addChild(loadingView);
+            await RES.loadConfig("resource/default.res.json", "resource/");
+            await this.loadTheme();
+            await RES.loadGroup("preload", 0, loadingView);
+            this.stage.removeChild(loadingView);
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+    private loadTheme() {
+        return new Promise<void>((resolve, reject) => {
+            // load skin theme configuration file, you can manually modify the file. And replace the default skin.
+            //加载皮肤主题配置文件,可以手动修改这个文件。替换默认皮肤。
+            let theme = new eui.Theme("resource/default.thm.json", this.stage);
+            theme.addEventListener(eui.UIEvent.COMPLETE, () => {
+                resolve();
+            }, this);
+
+        })
+    }
+    private textfield: egret.TextField;
+    /**
+     * 创建场景界面
+     * Create scene interface
+     */
+    protected createGameScene(): void {
+        let sky = this.createBitmapByName("bg_jpg");
+        this.addChild(sky);
+        let stageW = this.width;
+        let stageH = this.height;
+        sky.width = stageW;
+        sky.height = stageH;
+
+
+        //透明黑底条
+        // let topMask = new egret.Shape();
+        // topMask.graphics.beginFill(0x000000, 0.5);
+        // topMask.graphics.drawRect(0, 0, stageW, 172);
+        // topMask.graphics.endFill();
+        // topMask.y = 33;
+        // this.addChild(topMask);
+
+        // let icon: egret.Bitmap = this.createBitmapByName("egret_icon_png");
+        // this.addChild(icon);
+        // icon.x = 26;
+        // icon.y = 33;
+       
+        //白色竖条
+        let line = new egret.Shape();
+        line.graphics.lineStyle(2, 0xffffff);
+        line.graphics.moveTo(0, 0);
+        line.graphics.lineTo(0, 100);
+        line.graphics.endFill();
+        line.x = 200;
+        line.y = 100;
+        this.addChild(line);
+
+
+        // let colorLabel = new egret.TextField();
+        // colorLabel.textColor = 0xffffff;
+        // colorLabel.width = stageW - 172;
+        // colorLabel.textAlign = "center";
+        // colorLabel.text = "Hello Egret";
+        // colorLabel.size = 24;
+        // colorLabel.x = 172;
+        // colorLabel.y = 80;
+        // this.addChild(colorLabel);
+      
+        // /**
+        //  * 文本动画
+        //  */
+        // let textfield = new egret.TextField();
+        // this.addChild(textfield);
+        // textfield.alpha = 0;
+        // textfield.width = stageW - 172;
+        // textfield.textAlign = egret.HorizontalAlign.CENTER;
+        // textfield.size = 24;
+        // textfield.textColor = 0xffffff;
+        // textfield.x = 172;
+        // textfield.y = 135;
+        // this.textfield = textfield;
+
+        let startButton = new eui.Button();
+        let describeButton = new eui.Button();
+        startButton.label = "开始模拟";
+        describeButton.label = "模拟说明";
+        //位置
+        startButton.horizontalCenter = -250;
+        startButton.verticalCenter = 100;
+        describeButton.horizontalCenter = 250;
+        describeButton.verticalCenter = 100;
+        //尺寸
+        startButton.height = 50;
+        startButton.width = 120;
+        describeButton.height = 50;
+        describeButton.width = 120;
+        //添加到舞台
+        this.addChild(startButton);
+        this.addChild(describeButton);
+        //添加事件
+        startButton.addEventListener(egret.TouchEvent.TOUCH_TAP, this.onButtonClick, this);
+        
+    }
+    /**
+     * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
+     * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
+     */
+    private createBitmapByName(name: string): egret.Bitmap {
+        let result = new egret.Bitmap();
+        let texture: egret.Texture = RES.getRes(name);
+        result.texture = texture;
+        return result;
+    }
+    /**
+     * 描述文件加载成功，开始播放动画
+     * Description file loading is successful, start to play the animation
+     */
+    private startAnimation(result: Array<any>): void {
+        let parser = new egret.HtmlTextParser();
+
+        let textflowArr = result.map(text => parser.parse(text));
+        let textfield = this.textfield;
+        let count = -1;
+        let change = () => {
+            count++;
+            if (count >= textflowArr.length) {
+                count = 0;
+            }
+            let textFlow = textflowArr[count];
+
+            // 切换描述内容
+            // Switch to described content
+            textfield.textFlow = textFlow;
+            let tw = egret.Tween.get(textfield);
+            tw.to({ "alpha": 1 }, 200);
+            tw.wait(2000);
+            tw.to({ "alpha": 0 }, 200);
+            tw.call(change, this);
+        };
+        change();
+    }
+
+    /**
+     * 点击按钮
+     * Click the button
+     */
+    private onButtonClick(e: egret.TouchEvent) {
+        let firstScene = new FirstScene();
+        SceneManager.instance.pushScene(firstScene);
+    }
+    
+    public onComplete() {
+        egret.log("HOME界面加载完成");
+    }
+}
